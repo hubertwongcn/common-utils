9db3deded5f0b63880501b0934b940a955930878
wangh1906 <wangh1906@chinatelecom.cn>
2024-10-29 11:56:45 +0800

    Renaming packages for consistency
    Renamed Java packages to remove "utils" and align with the overall project structure. This enhances code readability and maintainability, ensuring a consistent package naming convention throughout the project.

---
+++ src/main/java/org/hubert/common/config/RocketMQCosnumerConfig.java
@@ -0,0 +1,36 @@
+package org.hubert.common.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.hubert.common.properties.PackageBindOpPropertiesConsumer;
+import org.hubert.common.properties.PackageBindOpPropertiesProducer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hubertwong
+ * @version 1.0
+ * @since 2024/10/4 14:50
+ */
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class RocketMQCosnumerConfig {
+
+    private final PackageBindOpPropertiesConsumer packageBindOpPropertiesConsumer;
+
+    @Bean(name = "packageBindOpConsumer")
+    public DefaultMQPushConsumer packageBindOpConsumer(PackageBindOpPropertiesProducer packageBindOpPropertiesProducer) {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(packageBindOpPropertiesConsumer.getGroup());
+        consumer.setNamesrvAddr(packageBindOpPropertiesConsumer.getNameServer());
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        try {
+            consumer.subscribe(packageBindOpPropertiesProducer.getTopic(), "*");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to subscribe to topic", e);
+        }
+        return consumer;
+    }
+}
